4189e5c37f6fc0cb17f4b23a89dae3d0d058fe23
Ofer Skolsky <Ofer.Skolsky@checkmarx.com>
2020-10-28 17:46:31 +0200

    add token

--- src/main/java/com/checkmarx/integrations/datastore/services/TokenService.java
+++ src/main/java/com/checkmarx/integrations/datastore/services/TokenService.java
@@ -1,11 +1,13 @@
 package com.checkmarx.integrations.datastore.services;
 
+import java.util.List;
+
 import com.checkmarx.integrations.datastore.models.Token;
 import com.checkmarx.integrations.datastore.repositories.ScmTokenRepository;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +18,4 @@ public class TokenService {
     public List<Token> getTokens() {
         return scmTokenRepository.findAll();
     }
-
-	public Token addToken(Token token) {
-		return scmTokenRepository.saveAndFlush(token);
-	}
 }
