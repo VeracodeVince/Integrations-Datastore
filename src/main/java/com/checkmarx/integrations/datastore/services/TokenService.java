dc2a4351ad428cd5ed5896e3c9c8e62c5308a95c
Ofer Skolsky <Ofer.Skolsky@checkmarx.com>
2020-10-28 21:47:00 +0200

    fix build

--- src/main/java/com/checkmarx/integrations/datastore/services/TokenService.java
+++ src/main/java/com/checkmarx/integrations/datastore/services/TokenService.java
@@ -1,13 +1,11 @@
 package com.checkmarx.integrations.datastore.services;
 
-import java.util.List;
-
 import com.checkmarx.integrations.datastore.models.Token;
 import com.checkmarx.integrations.datastore.repositories.ScmTokenRepository;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,4 +16,8 @@ public class TokenService {
     public List<Token> getTokens() {
         return scmTokenRepository.findAll();
     }
+
+	public Token addToken(Token token) {
+		return scmTokenRepository.saveAndFlush(token);
+	}
 }
