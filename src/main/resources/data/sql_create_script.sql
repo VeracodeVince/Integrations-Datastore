aa14a77a6d20a74c5d10b37028d427cc0a21b451
NimrodGolan <nimrod.golan@checkmarx.com>
2020-11-10 15:52:46 +0200

    Update create_script with new webhookId column

--- src/main/resources/data/sql_create_script.sql
+++ src/main/resources/data/sql_create_script.sql
@@ -5,9 +5,9 @@ drop table scms;
 
 CREATE TABLE scms
 (
-	id				SERIAL PRIMARY KEY,
-	base_url		varchar(50),
-	client_id		varchar(50),
+	id				    SERIAL PRIMARY KEY,
+	base_url		  varchar(50),
+	client_id		  varchar(50),
 	client_secret	varchar(50),
 	unique (base_url)
 
@@ -15,13 +15,13 @@ CREATE TABLE scms
 
 CREATE TABLE scm_orgs
 (
-	id				SERIAL PRIMARY KEY,
-	scm_id			int,
-	name			varchar(50),
-	cx_flow_url 	varchar(50),
+	id				      SERIAL PRIMARY KEY,
+	scm_id			    int,
+	name			      varchar(50),
+	cx_flow_url 	  varchar(50),
   cx_flow_config 	varchar(1000),
-	cx_go_token 	varchar(100),
-	team			varchar(50),
+	cx_go_token 	  varchar(100),
+	team			      varchar(50),
 	unique (scm_id, name),
 
 	CONSTRAINT fk_scm_orgs
@@ -31,10 +31,11 @@ CREATE TABLE scm_orgs
 
 CREATE TABLE scm_repos
 (
-	id						SERIAL PRIMARY KEY,
-	org_id					int,
-	name					varchar(50),
-	is_webhook_configured	BOOLEAN,
+	id						        SERIAL PRIMARY KEY,
+	org_id					      int,
+	name					        varchar(50),
+	webhook_id				    int,
+	is_webhook_configured	boolean,
 	unique (org_id, name),
 
 	CONSTRAINT fk_scm_repos
@@ -43,10 +44,10 @@ CREATE TABLE scm_repos
 );
 
 CREATE TABLE scm_tokens (
-	id				SERIAL PRIMARY KEY,
+	id				  SERIAL PRIMARY KEY,
 	org_id			int,
-	type			varchar(20),
-	token			varchar(100),
+	type			  varchar(20),
+	token			  varchar(100),
 	unique (org_id, type),
 
 	CONSTRAINT fk_tokens
