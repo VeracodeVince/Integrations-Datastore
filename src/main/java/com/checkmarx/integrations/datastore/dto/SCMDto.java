cf5e944c3b1c5fd3d6ad2d5f717e5f6b72e153bd
NimrodGolan <nimrod.golan@checkmarx.com>
2020-11-18 09:39:14 +0200

    Add SCMDto with Lombok Builder

--- src/main/java/com/checkmarx/integrations/datastore/dto/SCMDto.java
+++ src/main/java/com/checkmarx/integrations/datastore/dto/SCMDto.java
@@ -3,7 +3,10 @@ package com.checkmarx.integrations.datastore.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
@@ -13,6 +16,9 @@ import lombok.Data;
 })
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SCMDto {
 
     @JsonProperty("base_url")
