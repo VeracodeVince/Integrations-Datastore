775ec04cec451e6625d1cac5f4db6084b0a4f9e2
AsafCX <asaf.kalimi@checkmarx.com>
2020-11-30 14:48:33 +0200

    Add Scm controller tests

--- src/test/java/com/checkmarx/integrations/datastore/api/ScmApiSteps.java
+++ src/test/java/com/checkmarx/integrations/datastore/api/ScmApiSteps.java
@@ -1,35 +1,33 @@
 package com.checkmarx.integrations.datastore.api;
 
-import com.checkmarx.integrations.datastore.IntegrationsDataStoreApplication;
 import com.checkmarx.integrations.datastore.dto.SCMDto;
 import com.checkmarx.integrations.datastore.models.Scm;
 import com.checkmarx.integrations.datastore.repositories.ScmRepository;
 import io.cucumber.java.en.*;
 import io.cucumber.spring.CucumberContextConfiguration;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
 
 @Slf4j
 @CucumberContextConfiguration
-@SpringBootTest(classes = {IntegrationsDataStoreApplication.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ScmApiSteps {
 
-    private String urlPatternDataSourceGetScm = "http://localhost:8083/scms/%s";
-
-    private final ScmRepository scmRepository;
+    @LocalServerPort
+    private int port;
 
     private ResponseEntity<SCMDto> responseEntity;
 
     @Autowired
-    RestTemplate restTemplate;
+    private ScmRepository scmRepository;
 
-    public ScmApiSteps(
-            ScmRepository scmRepository) {
-        this.scmRepository = scmRepository;
-    }
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Given("data base contains {string}, {string} and {string}")
     public void initDataBaseValues(String baseUrl, String clientId, String clientSecret) {
@@ -42,19 +40,25 @@ public class ScmApiSteps {
 
     @When("API client calls the `get scm` API for {string}")
     public void apiClientCallsTheGetScmAPIFor(String scmBaseUrl) {
-        String path = String.format(urlPatternDataSourceGetScm, scmBaseUrl);
+        String path = String.format("http://localhost:%s/scms/%s", port, scmBaseUrl);
         responseEntity = restTemplate.getForEntity(path, SCMDto.class);
     }
 
     @Then("response status is {int}")
     public void responseStatusIs(int HttpStatusCode) {
+        Assert.assertEquals("Error HttpStatus", HttpStatusCode ,
+                            responseEntity.getStatusCodeValue());
     }
 
     @And("response contains the {string} field set to {string}")
-    public void responseContainsTheFieldSetTo(String baseUrl, String expectedBaseUrl) {
+    public void responseContainsTheFieldSetTo(String received, String expected) {
+        Assert.assertEquals(expected, received);
     }
 
-    @And("response does not have other fields")
-    public void responseDoesNotHaveOtherFields() {
+
+    @Given("data base does not contains {string} base url")
+    public void dataBaseDoesNotContainsBaseUrl(String baseUrl) {
+        Scm scm = scmRepository.getScmByBaseUrl(baseUrl);
+        Assert.assertNull(scm);
     }
 }
--- src/test/resources/cucumber/scm-api.feature
+++ src/test/resources/cucumber/scm-api.feature
@@ -4,9 +4,15 @@ Feature: SCM CRUD APIs
         Given data base contains "<scm base url>", "<Client Id>" and "<Client Secret>"
         When API client calls the `get scm` API for "<scm base url>"
         Then response status is 200
-        And response contains the "baseUrl" field set to "<scm base url>"
-        And response contains the "Client Id" field set to "<Client Id>"
-        And response contains the "Client Secret" field set to "<Client Secret>"
+        And response contains the "<scm base url>" field set to "<scm base url>"
+        And response contains the "<Client Id>" field set to "<Client Id>"
+        And response contains the "<Client Secret>" field set to "<Client Secret>"
         Examples:
             | scm base url    | Client Id             | Client Secret                            |
             | github.com      | ff718111a48803ba73566 | 7a260229fc9adf2c34f5e06e2b665f0f06094666 |
+            | gitlab.com      | ff718111a48803ba73562 | 7a260229fc9adf2c34f5e06e2b665f0f06094662 |
+
+    Scenario: Getting SCM details by a invalid SCM base url
+        Given data base does not contains "githubtest.com" base url
+        When API client calls the `get scm` API for "githubtest.com"
+        Then response status is 404
