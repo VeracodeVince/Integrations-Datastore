bbfa4db5d31e7f6b4b1ae09f9f58dbc42e6a8b12
AsafCX <asaf.kalimi@checkmarx.com>
2020-12-06 16:49:48 +0200

    Set application.properties port to 0 (Random)

--- src/test/java/com/checkmarx/integrations/datastore/api/shared/TestsSharedConfig.java
+++ src/test/java/com/checkmarx/integrations/datastore/api/shared/TestsSharedConfig.java
@@ -7,9 +7,6 @@ import io.cucumber.java.Before;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import java.util.Collections;
-import java.util.Random;
-
 public class TestsSharedConfig {
 
     private ConfigurableApplicationContext context;
@@ -17,10 +14,6 @@ public class TestsSharedConfig {
     @Before
     public void before() {
         SpringApplication springApplication=new SpringApplication(IntegrationsDataStoreApplication.class);
-        Random random = new Random();
-        int maxPort = 65535;
-        int randomPort = random.nextInt(maxPort);
-        springApplication.setDefaultProperties(Collections.singletonMap("server.port", randomPort));
         context =  springApplication.run();
     }
 
