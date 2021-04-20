b5f464608c3605339e63786ef4f56da96eb8dad2
AsafCX <72808004+AsafCX@users.noreply.github.com>
2021-04-20 09:12:02 +0300

    Default data insertion (Dev mode) (#21)

--- src/main/java/com/checkmarx/integrations/datastore/config/DataInitConfig.java
+++ src/main/java/com/checkmarx/integrations/datastore/config/DataInitConfig.java
@@ -1,10 +1,18 @@
 package com.checkmarx.integrations.datastore.config;
 
+import com.checkmarx.integrations.datastore.dto.AccessTokenShortDto;
+import com.checkmarx.integrations.datastore.dto.SCMOrgShortDto;
+import com.checkmarx.integrations.datastore.models.Scm;
 import com.checkmarx.integrations.datastore.repositories.ScmRepository;
+import com.checkmarx.integrations.datastore.services.OrgService;
+import com.checkmarx.integrations.datastore.services.TokenService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
@@ -17,6 +25,11 @@ public class DataInitConfig {
     private static final String GITLAB_REPO_BASE = "https://gitlab.com";
     private static final String AZURE_REPO_BASE = "https://dev.azure.com";
     private static final String BITBUCKET_REPO_BASE = "https://api.bitbucket.org";
+
+    private static final String GITHUB_DEFAULT_ORG = "GITHUB_DEFAULT_ORG";
+    private static final String GITHUB_DEFAULT_TOKEN = "GITHUB_DEFAULT_TOKEN";
+    private static final String ACCESS_TOKEN = "access_token";
+    private static final String REFRESH_TOKEN = "refresh_token";
 
     @Value("${github.client.sec}")
     private String githubClientSec;
@@ -36,9 +49,10 @@ public class DataInitConfig {
     @Value("${bitbucket.client.id}")
     private String bitbucketClientId;
 
+    private final Environment environment;
     private final ScmRepository scmRepository;
-
-
+    private final TokenService tokenService;
+    private final OrgService orgService;
 
     @PostConstruct
     public void dataInit(){
@@ -48,5 +62,45 @@ public class DataInitConfig {
         scmRepository.updateScmClientDetails(gitlabClientSec, gitlabClientId, GITLAB_REPO_BASE);
         scmRepository.updateScmClientDetails(azureClientSec, azureClientId, AZURE_REPO_BASE);
         scmRepository.updateScmClientDetails(bitbucketClientSec, bitbucketClientId, BITBUCKET_REPO_BASE);
+        if(isDevMode()) {
+            insertDefaultData();
+        }
+
+    }
+
+    private void insertDefaultData() {
+            insertGithubDefault();
+    }
+
+    private void insertGithubDefault() {
+        String orgIdentity = environment.getProperty(GITHUB_DEFAULT_ORG);
+        String orgToken =  environment.getProperty(GITHUB_DEFAULT_TOKEN);
+        Scm scm = scmRepository.getScmByRepoBaseUrl(GITHUB_REPO_BASE);
+
+        String tokenJson = getTokenJson(orgToken);
+        long tokenId = createDefaultToken(tokenJson);
+        createDefaultOrg(orgIdentity, scm, tokenId);
+    }
+
+    private void createDefaultOrg(String orgIdentity, Scm scm, long tokenId) {
+        orgService.createOrg(SCMOrgShortDto.builder().orgIdentity(orgIdentity).tokenId(tokenId).build(), scm
+                .getId());
+    }
+
+    private long createDefaultToken(String tokenJson) {
+        return tokenService.createTokenInfoIfDoesntExist(
+                AccessTokenShortDto.builder().accessToken(tokenJson).build());
+    }
+
+    private String getTokenJson(String orgToken) {
+        return new JSONObject()
+                .put(ACCESS_TOKEN, orgToken)
+                .put(REFRESH_TOKEN, "")
+                .toString();
+    }
+
+    private boolean isDevMode() {
+        return !StringUtils.isAnyEmpty(environment.getProperty(GITHUB_DEFAULT_ORG),
+                                      environment.getProperty(GITHUB_DEFAULT_TOKEN));
     }
 }
